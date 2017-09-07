48fb26f38bd5fec4791a3f4653c2bb1aaa4cc160
Dr. Asis Hallab <asis.hallab@gmail.com>
2017-09-07 13:56:56 +0000

    Ran 'dos2unix' on all non binary files

--- initUserDb.sh
+++ initUserDb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL 
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	CREATE USER kodiaq WITH SUPERUSER PASSWORD 'kodiaq';
 	CREATE DATABASE kodiaq_development OWNER kodiaq;
 	CREATE DATABASE kodiaq_test OWNER kodiaq;
